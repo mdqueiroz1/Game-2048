a71b9ff52295f32e29e18687d4ddf53a45ac1ef0
Matheus Queiroz <32309799+mdqueiroz1@users.noreply.github.com>
2018-09-21 10:19:37 -0300

    21/09

--- menu do jogo.c
+++ menu do jogo.c
@@ -4,38 +4,47 @@
 
 int main(){
 	
-	int menu1, volta=1;
+	int menu1, dificuldade;
 	
 	printf("--------------------------------\n");//30linhas
 	printf("--------------MENU--------------\n");
 	printf("--------------------------------\n");
-	printf("1- Novo Jogo\n");
+	printf("1- Iniciar Jogo\n");
 	printf("2- Ver Recordes\n");
 	printf("3- Definir Dificuldade\n");
-	printf("4- Continuar Jogo Salvo\n\n");
 	printf("--------------------------------\n");
 	
 	do{
 	scanf("%d", menu1);
-	}while(menu1<=0||menu1>5);
+	}while(menu1<=0||menu1>4);
 	
 	switch(menu1){
 		case 1:{
 			printf("Bem Vindo!");
+			printf("Informe o nome de usuário:\n");
+			
 			printf("Instruções: ... ... ...");
+			/*
+			w - cima 
+			a - esquerda
+			s - baixo
+			d - direita
+			*/
 			printf("Pressione enter para continuar.\n");
 			getch();
 			printf("jogo iniciado");//iniciar o jogo.
 			break;
 		}
 		case 2:{
+			printf("--------------------------------\n");
 			printf("Recordes:\n");
-			printf("1o - %s\t%d\n");//adicionar usuário para salvar o jogo e recorde do mesmo.
+			printf("1o - %s\t%d\n");//adicionar usuário do jogo já salvo e recorde do mesmo.
 			printf("2o - %s\t%d\n");
 			printf("3o - %s\t%d\n");
 			printf("Pressione enter para voltar.\n");
+			printf("--------------------------------\n");
 			getch();
-			printf("voltou.");//return menu_jogo;
+			printf("voltou");//return menu_jogo;
 			
 			break;
 		}
@@ -45,6 +54,17 @@ int main(){
 			printf("1- Facil\n");
 			printf("2- Medio\n");
 			printf("3- Dificil\n");
+			printf("--------------------------------\n");
+				
+				do{
+					printf("Selecione a dificuldade.\n");
+					scanf("%d",dificuldade);
+					if(dificuldade<=0||dificuldade>3){
+						printf("Numero de dificuldade invalido.\n");
+					}
+				}while(dificuldade<=0||dificuldade>3);
+				
+			
 			break;
 		}
 	}
