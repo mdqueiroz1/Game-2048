94003c4199107b3530c5c19055a4d821d5e6a149
Matheus Queiroz <32309799+mdqueiroz1@users.noreply.github.com>
2018-10-11 16:13:04 -0300

    11/10/18

--- teste.c
+++ teste.c
@@ -3,7 +3,6 @@
 #include<conio.h>
 
 /*
-   
    0_1_2_3_
 0 |2 0 4 2
 1 |0 0 0 0
@@ -13,9 +12,8 @@
 if(tabela[0][0]==tabela[0][1]){
 	tabela[0][0]=tabela[0][0]+tabela[0][1];
 }else tabela[0][0+1]
-
-
 */
+
 int main(){
 
 int lin,col,i,j,cont=0,tabela[4][4],cont_jogadas=0,pontuacao,inicio,r2ou4;
@@ -27,13 +25,14 @@ int lin,col,i,j,cont=0,tabela[4][4],cont_jogadas=0,pontuacao,inicio,r2ou4;
 	}
 	
 	srand(time(NULL));
-
-	do{
-		lin=rand()%4;
-		col=rand()%4;
-	}while(tabela[lin][col]!=0);
-
-	tabela[lin][col]=2;
+	for(i=0;i<2;i++){
+		do{
+			lin=rand()%4;
+			col=rand()%4;
+		}while(tabela[lin][col]!=0);
+		
+		tabela[lin][col]=2;
+	}
 
 	for(lin=0;lin<4;lin++){//impressao em tela
 		for(col=0;col<4;col++){
@@ -41,6 +40,7 @@ int lin,col,i,j,cont=0,tabela[4][4],cont_jogadas=0,pontuacao,inicio,r2ou4;
 		}
 		printf("\n");
 	}
+printf("\n");
 	
 	for(lin=0;lin<4;lin++){
 		while(col<3){
@@ -48,6 +48,12 @@ int lin,col,i,j,cont=0,tabela[4][4],cont_jogadas=0,pontuacao,inicio,r2ou4;
 				tabela[lin][col]=tabela[lin][col]+tabela[lin][col+1];
 				tabela[lin][col+1]=0;
 			}else col++;
+		}
+	}
+	
+	for(lin=0;lin<4;lin++){//impressao em tela
+		for(col=0;col<4;col++){
+			printf("%d",tabela[lin][col]);
 		}
 		printf("\n");
 	}
